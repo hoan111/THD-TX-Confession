f3317f2441f41cf7697023ab803231c53aaf00fa
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-05-18 23:00:49 +0700

    Ket noi csdl thong qua Mysqli

--- server/config.php
+++ server/config.php
@@ -1,3 +1,19 @@
 <?php  
+	//ket noi csdl MySQL thong qua MySQLi
+	$localhost = 'localhost';
+	$username = "root";
+	$password = "";
+	$database = "";
+
+	//doi tuong de ket noi
+	$mysqli = new mysqli($localhost, $username, $password, $database);
+
+	//thiet lap form chu tieng viet
+	$mysqli -> set_charset("utf8");
 	
+	//Thiet lap thong bao loi neu co
+	if(mysqli_connect_errno()){
+		echo "Khong the ket noi CSDL<br />".mysqli_connect_error();
+	exit();
+	}
 ?>
